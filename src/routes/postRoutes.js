35fac7a6a3e20edad74575d46274790b0539ad2b
Ruth Teshome <rteshome@charlotte.edu>
2026-02-17 18:05:24 -0500

    Added validation and sanitization for creating and updating posts.

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { getAllPostsHandler, getPostByIdHandler, createPostHandler, updatePostHandler, deletePostHandler } from '../controllers/postController.js';
-import { validateId } from '../middleware/postValidators.js';
+import { 
+    validateId, 
+    validateCreatePost, 
+    validateUpdatePost,
+} from '../middleware/postValidators.js';
 
 const router = express.Router();
 router.get('/', getAllPostsHandler);
 router.get('/:id', validateId, getPostByIdHandler);
-router.post('/', createPostHandler);
-router.put('/:id', updatePostHandler);
-router.delete('/:id', deletePostHandler)
+router.post('/', validateCreatePost, createPostHandler);
+router.put('/:id', validateId, validateUpdatePost, updatePostHandler);
+router.delete('/:id', validateId, deletePostHandler)
 
 export default router;
